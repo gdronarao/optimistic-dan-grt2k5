e45c33c3f86be218f63a3229f93953418216aae4
Drone <130157278+gdronarao@users.noreply.github.com>
2023-08-09 12:40:03 +0000

    Update 2 files

--- practice.py
+++ practice.py
@@ -1,7 +1,8 @@
 weight = input("weight:")
 unit = input("(k)g or (l)bs: ")
 if unit.upper() == "k":
-converted = weight / 0.45
-print("weight in (l)bs"+ converted)
+    converted = weight / 0.45
+    print("weight in Lbs: " + str(converted))
 else:
-    converted
+    converted = weight * 0.45
+    print("weight in kgs: " + (converted))
